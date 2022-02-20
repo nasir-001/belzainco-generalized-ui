88b3752928961f02f4b215720d9870aae853f31f
Nasir <nasirlawal001@gmail.com>
2022-02-20 11:28:29 +0000

    finish designing the services component

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,5 +1,105 @@
 <template>
-  <div>services</div>
+  <div class="row tw-justify-center tw-mt-56">
+    <div class="col-12 col-md-10 col-lg-8 col-xl-7">
+      <div class="tw-text-green-700 tw-text-4xl tw-text-center">
+        Our Services
+      </div>
+      <div class="tw-flex tw-justify-center tw-mt-16 tw-mb-10">
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/gettyimages-1129883796-612x612.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Agro Processing
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-pixabay-161556.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Ginger exporting
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/gettyimages-175453750-612x612.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Soya bean export
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-naman-nayar-922764.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Hibiscus flower
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/IMG_20210627_225625.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Potato flour
+          </div>
+        </div>
+      </div>
+      <div class="tw-flex tw-justify-center tw-mt-6 tw-mb-10">
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-eva-elijas-5988119.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Sesame seed
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-pixabay-39304.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Mango export
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/gettyimages-175453750-612x612.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Soya bean export
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-ylanite-koppens-915016.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Pineapple export
+          </div>
+        </div>
+        <div class="tw-w-56 tw-bg-white tw-h-64 tw-shadow-lg tw-mx-2">
+          <q-img
+            class="tw-h-52 tw-rounded-t-xl tw-object-cover"
+            src="~assets/img/pexels-wendy-wei-1656666.jpg"
+          />
+          <div class="tw-text-green-700 tw-text-base tw-text-center tw-pt-2">
+            Avocado export
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
